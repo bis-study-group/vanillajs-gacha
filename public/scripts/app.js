5d5811ee53e6237440cfbfd696364a0a883b50e8
Hiromichi Yamamoto <hiromichi.yamamoto@ari-jp.com>
2018-12-23 14:17:26 +0900

    猫GIFガチャを作ろう

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,16 @@
-const playButton = document.getElementById('play');
-const resultBlock = document.getElementById('result');
-const againButton = document.getElementById('again');
-const gifImg = document.getElementById('gif');
+const playButton = document.getElementById("play");
+const resultBlock = document.getElementById("result");
+const againButton = document.getElementById("again");
+const gifImg = document.getElementById("gif");
 
-playButton.addEventListener('click', function() {
-  axios.get('/api/gacha')
-    .then(response => {
-      // Insert code here...
-    });
+playButton.addEventListener("click", function() {
+  axios.get("/api/gacha").then(response => {
+    resultBlock.classList.remove("hidden");
+    gifImg.src = response.data.url;
+  });
 });
 
-againButton.addEventListener('click', function() {
-  resultBlock.classList.add('hidden');
-  playButton.classList.remove('hidden');
+againButton.addEventListener("click", function() {
+  resultBlock.classList.add("hidden");
+  playButton.classList.remove("hidden");
 });
