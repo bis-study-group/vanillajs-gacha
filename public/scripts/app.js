bd669f489da7bc008726952b85125465f5c5ae0e
yuma.kadoi <kadoi82uma@gmail.com>
2019-02-12 00:17:36 +0900

    課題4

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,17 @@
-const playButton = document.getElementById('play');
-const resultBlock = document.getElementById('result');
-const againButton = document.getElementById('again');
-const gifImg = document.getElementById('gif');
+const playButton = document.getElementById("play");
+const resultBlock = document.getElementById("result");
+const againButton = document.getElementById("again");
+const gifImg = document.getElementById("gif");
 
-playButton.addEventListener('click', function() {
-  axios.get('/api/gacha')
-    .then(response => {
-      // Insert code here...
-    });
+playButton.addEventListener("click", function() {
+  axios.get("/api/gacha").then(response => {
+    gifImg.src = response.data.url;
+    resultBlock.classList.remove("hidden");
+    playButton.classList.add("hidden");
+  });
 });
 
-againButton.addEventListener('click', function() {
-  resultBlock.classList.add('hidden');
-  playButton.classList.remove('hidden');
+againButton.addEventListener("click", function() {
+  resultBlock.classList.add("hidden");
+  playButton.classList.remove("hidden");
 });
